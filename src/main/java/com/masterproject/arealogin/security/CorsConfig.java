266480c7bdb600fb813c210f26152bbef5d3b6e2
Gean <gean0606@hotmail.com>
2025-07-17 15:49:52 -0300

    Permitindo acesso do site principal

--- src/main/java/com/masterproject/arealogin/security/CorsConfig.java
+++ src/main/java/com/masterproject/arealogin/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Aplica-se a todos os endpoints sob /api
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500") // Para teste local com Live Server
+                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "https://www.masterproject.com.br", "http,://www.masterproject.com.br")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
