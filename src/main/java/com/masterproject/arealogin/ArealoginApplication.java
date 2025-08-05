f93c7eb39fc99152dc4aa1579a54b51b092c76c9
Gean <gean0606@hotmail.com>
2025-08-05 13:56:40 -0300

    Fix: Removendo bean PasswordEncoder duplicado

--- src/main/java/com/masterproject/arealogin/ArealoginApplication.java
+++ src/main/java/com/masterproject/arealogin/ArealoginApplication.java
@@ -12,8 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.util.List;
 
 @SpringBootApplication
